c9e523100e27b6bc4950b990857dc2f14ca4d59f
Grant Hamm <tghamm@gmail.com>
2022-03-07 08:51:15 -0500

    adds simple ORM test suite

---
+++ Castle.DynamicLinqQueryBuilder.Tests31/Database/StoreContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Castle.DynamicLinqQueryBuilder.Tests31.Database
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext>
+    {
+        public StoreContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+            optionsBuilder.UseSqlite("Filename=:memory:");
+
+            return new StoreContext(optionsBuilder.Options);
+        }
+    }
+
+    public class Store
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid StoreId { get; set; }
+
+        public string StoreName { get; set; }
+        public DateTime OpenDate { get; set; }
+        public double TotalRevenue { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        [ForeignKey("Store")]
+        public Guid StoreId { get; set; }
+
+        public virtual Store Store { get; set; }
+
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+
+    }
+}
---
+++ Castle.DynamicLinqQueryBuilder.Tests31/ORM/ORMTests.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicLinqQueryBuilder.Tests31.Database;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Castle.DynamicLinqQueryBuilder.Tests31.ORM
+{
+    [TestFixture]
+    public class ORMTests: IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private readonly DbContextOptions<StoreContext> _contextOptions;
+
+        public ORMTests()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _contextOptions = new DbContextOptionsBuilder<StoreContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            // Create the schema and seed some data
+            using var context = new StoreContext(_contextOptions);
+
+            context.Database.EnsureCreated();
+
+
+            var stores = new List<Store>()
+            {
+                new Store()
+                {
+                    StoreId = Guid.NewGuid(),
+                    OpenDate = new DateTime(2020, 12, 18),
+                    StoreName = "Tractor Store",
+                    TotalRevenue = 200000000,
+                    Products = new List<Product>()
+                    {
+                        new Product()
+                        {
+                            ProductName = "Model 12 Tractor",
+                            ProductPrice = 75000
+                        },
+                        new Product()
+                        {
+                            ProductName = "Model 14 Tractor",
+                            ProductPrice = 122000
+                        },
+                        new Product()
+                        {
+                            ProductName = "Plow",
+                            ProductPrice = 1200
+                        }
+                    }
+                },
+                new Store()
+                {
+                    StoreId = Guid.NewGuid(),
+                    OpenDate = new DateTime(2021, 12, 15),
+                    StoreName = "Tire Store",
+                    TotalRevenue = 150000000,
+                    Products = new List<Product>()
+                    {
+                        new Product()
+                        {
+                            ProductName = "Tractor Tire",
+                            ProductPrice = 12000
+                        },
+                        new Product()
+                        {
+                            ProductName = "Car Tire",
+                            ProductPrice = 120
+                        },
+                        new Product()
+                        {
+                            ProductName = "Cap",
+                            ProductPrice = 75
+                        }
+                    }
+                }
+            };
+        
+            context.AddRange(stores);
+            context.SaveChanges();
+
+        }
+
+        StoreContext CreateContext() => new StoreContext(_contextOptions);
+        
+        public void Dispose() => _connection.Dispose();
+
+        [Test]
+        public void ContainsTractorTest()
+        {
+            var context = CreateContext();
+
+            //expect 3 entries to match for an contains Tractor comparison
+            var tractorFilter = new QueryBuilderFilterRule
+            {
+                Condition = "and",
+                Rules = new List<QueryBuilderFilterRule>
+                {
+                    new QueryBuilderFilterRule
+                    {
+                        Condition = "and",
+                        Field = "ProductName",
+                        Id = "ProductName",
+                        Input = "NA",
+                        Operator = "contains",
+                        Type = "string",
+                        Value = new[] { "Tractor" }
+                    }
+                }
+            };
+
+            var tractorIdFilteredList = context.Products.BuildQuery(tractorFilter).ToList();
+            Assert.IsTrue(tractorIdFilteredList != null);
+            Assert.IsTrue(tractorIdFilteredList.Count == 3);
+        }
+
+        [Test]
+        public void BeginsWithModelTest()
+        {
+            var context = CreateContext();
+
+            //expect 2 entries to match for a begins with comparison
+            var modelFilter = new QueryBuilderFilterRule
+            {
+                Condition = "and",
+                Rules = new List<QueryBuilderFilterRule>
+                {
+                    new QueryBuilderFilterRule
+                    {
+                        Condition = "and",
+                        Field = "ProductName",
+                        Id = "ProductName",
+                        Input = "NA",
+                        Operator = "begins_with",
+                        Type = "string",
+                        Value = new[] { "Model" }
+                    }
+                }
+            };
+
+            var beginsFilteredList = context.Products.BuildQuery(modelFilter).ToList();
+            Assert.IsTrue(beginsFilteredList != null);
+            Assert.IsTrue(beginsFilteredList.Count == 2);
+        }
+
+        [Test]
+        public void EqualsTireStoreTest()
+        {
+            var context = CreateContext();
+
+            //expect 1 entry to match for an contains Tire Store comparison
+            var storeFilter = new QueryBuilderFilterRule
+            {
+                Condition = "and",
+                Rules = new List<QueryBuilderFilterRule>
+                {
+                    new QueryBuilderFilterRule
+                    {
+                        Condition = "and",
+                        Field = "StoreName",
+                        Id = "StoreName",
+                        Input = "NA",
+                        Operator = "equal",
+                        Type = "string",
+                        Value = new[] { "Tire Store" }
+                    }
+                }
+            };
+
+            var storeFilteredList = context.Stores.BuildQuery(storeFilter).ToList();
+            Assert.IsTrue(storeFilteredList != null);
+            Assert.IsTrue(storeFilteredList.Count == 1);
+        }
+
+
+    }
+}
