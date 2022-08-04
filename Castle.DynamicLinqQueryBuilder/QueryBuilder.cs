1df4737b1704cc3ab4a95df12988cfca7a4d7e40
fyzact <feyyazacet@gmail.com>
2022-08-04 14:46:56 +0100

    nested nullcheck only for custom types

--- Castle.DynamicLinqQueryBuilder/QueryBuilder.cs
+++ Castle.DynamicLinqQueryBuilder/QueryBuilder.cs
@@ -308,7 +308,7 @@ namespace Castle.DynamicLinqQueryBuilder
                         predicate
                     ));
                 }
-                else if (!expression.Type.IsValueType)
+                else if (!expression.Type.IsValueType && propertyType != typeof(string))
                 {
                     var notnull = IsNotNull(expression);
                     Expression body = BuildNestedExpression(expression, propertyCollectionEnumerator, rule, options, type);
